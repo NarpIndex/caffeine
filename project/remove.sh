19ff22b4f6018d818c4bcc4c5c16f9b165c2b86f
Leon <132808925+Leon8326@users.noreply.github.com>
2025-05-17 18:43:04 +0200

    Update remove.sh

--- project/remove.sh
+++ project/remove.sh
@@ -4,6 +4,6 @@ sudo rm -rf /usr/bin/caffeine
 for dir in /home/*; do
   if [[ -d "$dir/.caffeine" ]]; then
     sudo rm -rf "$dir/.caffeine"
-    echo "✅ Removed Caffeine from $dir"
+    echo "✅ Removed Caffeine from $dir."
   fi
 done
